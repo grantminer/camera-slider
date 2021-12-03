ab33e4a7adfc5114a05c2120d1dfb0381bd0c549
Grant <gminer@olin.edu>
2021-12-03 13:31:30 -0500

    Add microstep functionality and debug direction problems

--- motor_control_speedy/motor_control_speedy.ino
+++ motor_control_speedy/motor_control_speedy.ino
@@ -5,50 +5,55 @@ SpeedyStepper panStepper;
 
 #define dirPin1 2
 #define stepPin1 3
-#define dirPin2 4
-#define stepPin2 5
+//#define dirPin2 4
+//#define stepPin2 5
 #define LIMIT_SWITCH_PIN 6
 
 int maxRevs = 16;
 
 int speedCounterTranslate = 10;
 int positionCounterTranslate = 20;
-int speedCounterPan = 5;
-int positionCounterPan = 1;
+int maxPosition = 100;
+bool homed = false;
+//int speedCounterPan = 5;
+//int positionCounterPan = 1;
 
 void setup() {
   // put your setup code here, to run once:
 
   translateStepper.connectToPins(stepPin1, dirPin1);
-  panStepper.connectToPins(stepPin2, dirPin2);
+//  panStepper.connectToPins(stepPin2, dirPin2);
   pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
+  translateStepper.setStepsPerRevolution(1600);
+  translateStepper.setAccelerationInRevolutionsPerSecondPerSecond(1);
   translateStepper.setSpeedInRevolutionsPerSecond(speedCounterTranslate/10);
-  panStepper.setSpeedInRevolutionsPerSecond(speedCounterPan/10);
-  
-  translateStepper.moveToHomeInRevolutions(-1, 1, 16, LIMIT_SWITCH_PIN);
+//  panStepper.setSpeedInRevolutionsPerSecond(speedCounterPan/10);
+
+  Serial.println(translateStepper.getCurrentPositionInRevolutions());
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (!homed) {
+    homed = translateStepper.moveToHomeInRevolutions(-1, 1, 16, LIMIT_SWITCH_PIN);
+  }
 
-  translateStepper.setupMoveInRevolutions((positionCounterTranslate/20)*maxRevs);
-  panStepper.setupRelativeMoveInRevolutions(0.5);
+// while(!go) {
+//
+//while(true){}
+
+  translateStepper.setupMoveInRevolutions((positionCounterTranslate/maxPosition)*maxRevs);
+//  panStepper.setupRelativeMoveInRevolutions(0.5);
 
   while((!translateStepper.motionComplete()) || (!panStepper.motionComplete())) {
+    Serial.println(translateStepper.getCurrentPositionInRevolutions());
     translateStepper.processMovement();
     panStepper.processMovement();
+    homed = false;
   }
 
   delay(2000);
 
-  translateStepper.setupMoveInRevolutions(0);
-  panStepper.setupRelativeMoveInRevolutions(-0.5);
-
-  while((!translateStepper.motionComplete()) || (!panStepper.motionComplete())) {
-    translateStepper.processMovement();
-    panStepper.processMovement();
-  }
-
-  delay(10000);
+// while(!home)
 }
