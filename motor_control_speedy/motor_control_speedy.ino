b3a2c18c46ccf68cb77062d5c5ce5635b5a8ef56
Grant <gminer@olin.edu>
2021-12-08 15:59:03 -0500

    Recalibrate translate stepper, attempt to test with both connected

--- motor_control_speedy/motor_control_speedy.ino
+++ motor_control_speedy/motor_control_speedy.ino
@@ -5,31 +5,39 @@ SpeedyStepper panStepper;
 
 #define dirPin1 2
 #define stepPin1 3
-//#define dirPin2 4
-//#define stepPin2 5
+#define dirPin2 4
+#define stepPin2 5
 #define LIMIT_SWITCH_PIN 6
 
 int maxDist = 1.5; // Meters
 
-int speedCounterTranslate = 10;
+int speedCounterTranslate = 100;
 int positionCounterTranslate = 20;
 int maxPosition = 100;
 bool homed = false;
 int speedCounterPan = 5;
 int positionCounterPan = 1;
 
+int maxTranslatePosition = 120;
+int maxPanPosition = 180;
+int maxTranslateSpeed = 100;
+int maxPanSpeed = 1;
+
 void setup() {
   // put your setup code here, to run once:
 
   if (speedCounterPan >= 1) {speedCounterPan = 1;}
-  if (speedCounterTranslate >= 
+  if (speedCounterTranslate >= maxTranslateSpeed) {speedCounterTranslate = maxTranslateSpeed;}
+
+  if (positionCounterTranslate >= maxTranslatePosition) {positionCounterTranslate = maxTranslatePosition;}
+  if (positionCounterPan >= maxPanPosition) {positionCounterPan = maxPanPosition;}
 
   translateStepper.connectToPins(stepPin1, dirPin1);
   panStepper.connectToPins(stepPin2, dirPin2);
   pinMode(LIMIT_SWITCH_PIN, INPUT_PULLUP);
-  translateStepper.setStepsPerMillimeter(0.04);
-  translateStepper.setAccelerationInRevolutionsPerSecondPerSecond(1);
-  translateStepper.setSpeedInRevolutionsPerSecond(speedCounterTranslate/10);
+  translateStepper.setStepsPerMillimeter(20);
+  translateStepper.setAccelerationInMillimetersPerSecondPerSecond(30);
+  translateStepper.setSpeedInMillimetersPerSecond(speedCounterTranslate);
   panStepper.setSpeedInRevolutionsPerSecond(speedCounterPan*4);
 
   Serial.println(translateStepper.getCurrentPositionInMillimeters()*1000);
@@ -39,18 +47,18 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (!homed) {
-    homed = translateStepper.moveToHomeInRevolutions(-1, 1, 16, LIMIT_SWITCH_PIN);
+    homed = translateStepper.moveToHomeInMillimeters(-1, 50, 1300, LIMIT_SWITCH_PIN);
   }
 
 // while(!go) {
 //
 //while(true){}
 
-  translateStepper.setupMoveInMillimeters((positionCounterTranslate/maxPosition)*maxRevs*1000);
-  panStepper.setupRelativeMoveInRevolutions((positionCounterPan/90));
+  translateStepper.setupMoveInMillimeters(positionCounterTranslate);
+  panStepper.setupRelativeMoveInRevolutions(positionCounterPan);
 
   while((!translateStepper.motionComplete()) || (!panStepper.motionComplete())) {
-    Serial.println(translateStepper.getCurrentPositionInMillimeters()*1000);
+    Serial.println(translateStepper.getCurrentPositionInMillimeters()/10);
     translateStepper.processMovement();
     panStepper.processMovement();
     homed = false;
